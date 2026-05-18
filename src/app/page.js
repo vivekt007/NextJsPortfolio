6bdd74b584aea62bc4613fff9941fe9f29b3a8ea
Vivek Tigadi <vivektigadi59@gmail.com>
2026-05-18 15:33:00 +0530

    Map update

--- src/app/page.js
+++ src/app/page.js
@@ -265,15 +265,7 @@ export default function Home() {
                     <p>MAP</p>
                   </div>
                 </div>
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d236860.71332812856!2d77.46612573821041!3d12.954280236682722!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka!5e1!3m2!1sen!2sin!4v1745168872219!5m2!1sen!2sin"
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  allowFullScreen=""
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18134459.285238214!2d69.62778194042443!3d19.124044554222664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1779098498938!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               </div>
             </div>
 
